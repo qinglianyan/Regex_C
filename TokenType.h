875de0f44674817ffacbabd490baddc46959b05b
qinglianyan <921049836@qq.com>
2023-11-17 10:18:07 +0800

    tokenType

---
+++ TokenType.h
@@ -0,0 +1,20 @@
+
+enum TokenTypes{
+    NONE, 
+    BACKSLASH,      //"\"
+
+    POINT,          //"."
+    CARET,          //"^"
+    DOLLOR,         //"$"
+    LBRACE,         //"("
+    RBRACE,         //")"
+    LBRACKET,       //"["
+    RBRACKET,       //"]"
+    LPARENT,        //"{"
+    RPARENT,        //"}"
+    STAR,           //"*"
+    MINUS,          //"-"
+    QMARK,          //"?"
+    OR,             //"|"
+    PLUS,           //"+"
+};
---
+++ token.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "TokenType.h"
+
+int main(){
+
+    return 0;
+}
